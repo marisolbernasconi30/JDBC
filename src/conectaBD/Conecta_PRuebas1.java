c8627597659f4a21be929550353621d46b6c7167
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-07-28 18:25:26 -0300

    falta que funcione

--- src/conectaBD/Conecta_PRuebas1.java
+++ src/conectaBD/Conecta_PRuebas1.java
@@ -7,10 +7,26 @@ public class Conecta_PRuebas1 {
         
         try {
 
+             // PASO 1: CARGAMOS EL DRIVER
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
             //PASO 1: CREAMOS LA CONEXION
             Connection miconexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "newyorkcode");
+
+            //PASO 2: CREAMOS EL STATEMENT
+            Statement mystatement=miconexion.createStatement();
+
+            //PASO 3: EJECUTAMOS LA CONSULTA SQL 
+            ResultSet myresultset=mystatement.executeQuery("SELECT * FROM PRODUCTOS");
+
+            //PASO 4: RECORREMOS EL RESULTSET
+            while(myresultset.next()) { //MIENTRAS HAYA UN REGISTRO MAS HACIA ADELANTE DEL CURSOR
+                System.out.println(myresultset.getString("CODIGOPRODUCTO") + " - " + myresultset.getString("NOMBREARTICULO") + " - " + myresultset.getString("PAISORIGEN")); //PARA QUE NOS DEVUELVA EL CONTENIDO DE CADA COLUMNA DEL REGISTRO
+            }
+
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println("ERROR, NO CONECTA!");
+            e.printStackTrace(); //IMPRIME EL ERROR EN PANTALLA
         }
 
         
