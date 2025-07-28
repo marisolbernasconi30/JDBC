507296deea67201cd90f4efe57dba76a5c499edb
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-07-28 15:09:49 -0300

    submodulo

--- src/conectaBD/Conecta_PRuebas1.java
+++ src/conectaBD/Conecta_PRuebas1.java
@@ -1,7 +1,18 @@
 package conectaBD;
+import java.sql.*;
+import javax.sql.*;
 
 public class Conecta_PRuebas1 {
     public static void main(String[] args) {
+        
+        try {
+
+            //PASO 1: CREAMOS LA CONEXION
+            Connection miconexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "newyorkcode");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
         
     }
 }
