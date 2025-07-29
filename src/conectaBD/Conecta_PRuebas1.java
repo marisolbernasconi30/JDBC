7cda29701fcb5e25005d63366017a4fc55804349
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-07-29 10:40:19 -0300

    funciona

--- src/conectaBD/Conecta_PRuebas1.java
+++ src/conectaBD/Conecta_PRuebas1.java
@@ -3,7 +3,10 @@ import java.sql.*;
 import javax.sql.*;
 
 public class Conecta_PRuebas1 {
-    public static void main(String[] args) {
+
+    
+    public static void main(String[] args){
+        
         
         try {
 
@@ -11,7 +14,7 @@ public class Conecta_PRuebas1 {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             //PASO 1: CREAMOS LA CONEXION
-            Connection miconexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "newyorkcode");
+            Connection miconexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "");
 
             //PASO 2: CREAMOS EL STATEMENT
             Statement mystatement=miconexion.createStatement();
@@ -21,7 +24,7 @@ public class Conecta_PRuebas1 {
 
             //PASO 4: RECORREMOS EL RESULTSET
             while(myresultset.next()) { //MIENTRAS HAYA UN REGISTRO MAS HACIA ADELANTE DEL CURSOR
-                System.out.println(myresultset.getString("CODIGOPRODUCTO") + " - " + myresultset.getString("NOMBREARTICULO") + " - " + myresultset.getString("PAISORIGEN")); //PARA QUE NOS DEVUELVA EL CONTENIDO DE CADA COLUMNA DEL REGISTRO
+                System.out.println(myresultset.getString("CODIGOARTICULO") + " - " + myresultset.getString("NOMBREARTICULO") + " - " + myresultset.getString("PAISORIGEN")); //PARA QUE NOS DEVUELVA EL CONTENIDO DE CADA COLUMNA DEL REGISTRO
             }
 
         } catch (Exception e) {
@@ -31,4 +34,16 @@ public class Conecta_PRuebas1 {
 
         
     }
+
 }
+
+/*
+ * NO SE POR QUE, PERO SI YO COMPILO ESTO EN LA TERMINAL:
+ * javac -d bin -cp "lib/mysql-connector-java-9.4.0.jar" src/conectaBD/Conecta_PRuebas1.java
+ * Y LUEGO EJECUTO:
+ * java -cp "bin:lib/mysql-connector-java-9.4.0.jar" conectaBD.Conecta_PRuebas1
+ * AHI SI FUNCIONA. 
+ * PERO SI LO HAGO DESDE EL "RUN CODE" NO FUNCIONA. 
+ */
+
+
