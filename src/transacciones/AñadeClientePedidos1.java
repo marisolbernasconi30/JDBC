b96dc2ecd87c7332ff1c942a9fa1c75db9bc52cb
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-08-14 18:29:11 -0300

    adelantos

---
+++ src/transacciones/AñadeClientePedidos1.java
@@ -0,0 +1,56 @@
+package transacciones;
+
+import java.sql.*;
+
+public class AñadeClientePedidos1 {
+    
+    public static void main(String[] args) {
+        
+
+        try {
+
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "");
+
+            Statement miSentencia = conexion.createStatement();
+
+            // Iniciar la transacción
+            String sentenciaSQL1="INSERT INTO CLIENTES (CODIGOCLIENTE, EMPRESA, DIRECCION, POBLACION, TELEFONO, RESPONSABLE, HISTORIAL) VALUES ('CL023', 'Ejemplo', 'Calle Falsa 123', 'MADRID', '856932147', 'MONICA IBAÑEZ', '45 COMPRAS')";
+            miSentencia.executeUpdate(sentenciaSQL1);
+
+            String sentenciaSQL2="INSERT INTO PEDIDOS (NUMEROPEDIDO, CODIGOCLIENTE, FECHAPEDIDO, FORMAPAGO, DESCUENTO, ENVIADO) VALUES ('023', 'CL023', '2024-06-20', 'TRANSFERENCIA', '0.15', 'FALSO')";
+            miSentencia.executeUpdate(sentenciaSQL2);
+            
+            System.out.println("Cliente y pedido añadidos correctamente.");
+        
+            
+                
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            System.out.println("No se ha podido añadir el cliente y el pedido.");
+
+        }
+
+/*
+*  pwd
+*
+* cd /home/marisol/eclipse-workspace/PROYECTO/JDBC/JavaDataBaseConnectivity
+*
+* ls lib/mysql-connector-java-9.4.0.jar
+*
+* ls ~/eclipse-workspace/PROYECTO/JDBC/JavaDataBaseConnectivity/lib/mysql-connector-java-9.4.0.jar
+*
+# Compilar todo
+javac -d bin -cp "lib/mysql-connector-java-9.4.0.jar" $(find src -name "*.java")
+
+# Ejecutar
+
+java -cp "bin:lib/mysql-connector-java-9.4.0.jar" transacciones.AñadeClientePedidos1
+
+*/
+
+
+
+
+    }
+}
