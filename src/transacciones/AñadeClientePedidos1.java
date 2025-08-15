bb62df5c31492269af38dbd8b754062d72faa79b
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-08-15 10:57:16 -0300

    aclaraciones

--- src/transacciones/AñadeClientePedidos1.java
+++ src/transacciones/AñadeClientePedidos1.java
@@ -13,14 +13,23 @@ public class AñadeClientePedidos1 {
 
             Statement miSentencia = conexion.createStatement();
 
-            // Iniciar la transacción
+            // SUPONGAMOS QUE YO LO HAGO ASÍ:
+            //ESTA SENTENCIA ESTÁ BIEN ESCRITA.
             String sentenciaSQL1="INSERT INTO CLIENTES (CODIGOCLIENTE, EMPRESA, DIRECCION, POBLACION, TELEFONO, RESPONSABLE, HISTORIAL) VALUES ('CL023', 'Ejemplo', 'Calle Falsa 123', 'MADRID', '856932147', 'MONICA IBAÑEZ', '45 COMPRAS')";
             miSentencia.executeUpdate(sentenciaSQL1);
 
-            String sentenciaSQL2="INSERT INTO PEDIDOS (NUMEROPEDIDO, CODIGOCLIENTE, FECHAPEDIDO, FORMAPAGO, DESCUENTO, ENVIADO) VALUES ('023', 'CL023', '2024-06-20', 'TRANSFERENCIA', '0.15', 'FALSO')";
+            //ESTA SENTENCIA ESTÁ MAL ESCRITA PORQUE ESTÁ MAL EL NOMBRE DE LA COLUMNA (VA SIN EL _)
+            String sentenciaSQL2="INSERT INTO PEDIDOS (NUMERO_PEDIDO, CODIGOCLIENTE, FECHAPEDIDO, FORMAPAGO, DESCUENTO, ENVIADO) VALUES ('023', 'CL023', '2024-06-20', 'TRANSFERENCIA', '0.15', 'FALSO')";
             miSentencia.executeUpdate(sentenciaSQL2);
             
             System.out.println("Cliente y pedido añadidos correctamente.");
+
+            /*
+             * ME VA A DAR ERROR EN LA SEGUNDA SENTENCIA PORQUE ESTÁ MAL ESCRITO
+             * PERO LA PRIMER SENTENCIA SE VA A EJECUTAR CORRECTAMENTE. 
+             * ESTO, NO ES UNA TRANSACTION.
+             * EN UNA TRANSACTION, SI UNA FALLA, TODAS FALLAN.
+             */
         
             
                 
