7de14e7bf31cee1ff15e40a17b483651d47d2d5c
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-08-18 13:54:14 -0300

    inicio metadatos

---
+++ src/metadatos/InfoMetadatos1.java
@@ -0,0 +1,29 @@
+package metadatos;
+
+public class InfoMetadatos1 {
+
+    public static void main(String[] args) {
+         
+    }
+
+}
+/*
+
+PROBAR SI ESTO FUNCIONA 
+
+*  pwd
+*
+* cd /home/marisol/eclipse-workspace/PROYECTO/JDBC/JavaDataBaseConnectivity
+*
+* ls lib/mysql-connector-java-9.4.0.jar
+*
+* ls ~/eclipse-workspace/PROYECTO/JDBC/JavaDataBaseConnectivity/lib/mysql-connector-java-9.4.0.jar
+*
+# Compilar todo
+javac -d bin -cp "lib/mysql-connector-java-9.4.0.jar" $(find src -name "*.java")
+
+# Ejecutar
+
+java -cp "bin:lib/mysql-connector-java-9.4.0.jar" metadatos.InfoMetadatos1
+
+*/
