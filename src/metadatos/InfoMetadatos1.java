c71ff190c40759ceeb466611adfefd5e57c8fbae
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-08-18 17:43:44 -0300

    funciona metadato basico

--- src/metadatos/InfoMetadatos1.java
+++ src/metadatos/InfoMetadatos1.java
@@ -1,12 +1,54 @@
 package metadatos;
 
+import java.sql.*;
+
 public class InfoMetadatos1 {
 
     public static void main(String[] args) {
-         
+     
+        mostrarInfoBBDD();
+    }
+
+    static void mostrarInfoBBDD(){
+        Connection miconexion=null;
+
+        try{
+            
+            // Establecer la conexión
+            miconexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "");
+
+            // Obtener los metadatos de la conexión
+            DatabaseMetaData metadatos = miconexion.getMetaData();
+
+            // Mostrar información de la base de datos
+            System.out.println("Gestor de BBDD: " + metadatos.getDatabaseProductName());
+            System.out.println("Versión del gestor: " + metadatos.getDatabaseProductVersion());
+            System.out.println("Nombre del driver: " + metadatos.getDriverName());
+            System.out.println("Versión del driver: " + metadatos.getDriverVersion());
+            //System.out.println("Usuario conectado: " + metadatos.getUserName());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally{
+            try {
+                miconexion.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        
     }
 
 }
+
+
+
+
+
+
+
+
 /*
 
 PROBAR SI ESTO FUNCIONA 
