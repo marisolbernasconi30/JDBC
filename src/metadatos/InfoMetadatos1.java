ae81f8a81a5c1fd7b10df5daaa0f3e37c6350cc4
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-08-19 20:08:38 -0300

    Metadato: columna y tabla

--- src/metadatos/InfoMetadatos1.java
+++ src/metadatos/InfoMetadatos1.java
@@ -6,7 +6,9 @@ public class InfoMetadatos1 {
 
     public static void main(String[] args) {
      
-        mostrarInfoBBDD();
+       // mostrarInfoBBDD();
+        mostrarInfoTablas();
+
     }
 
     static void mostrarInfoBBDD(){
@@ -38,6 +40,49 @@ public class InfoMetadatos1 {
         }
 
         
+    }
+
+    static void mostrarInfoTablas(){
+        Connection miconexion=null;
+        ResultSet rs=null;
+         ResultSet rs1=null;
+
+        try{
+            // Establecer la conexión
+            miconexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "");
+
+            // Obtener los metadatos de la conexión
+            DatabaseMetaData metadatos = miconexion.getMetaData();
+
+            // Obtener las tablas de la base de datos
+            rs = metadatos.getTables(null, null, null, null);
+            //si en el tercer parámetro le pongo "p%" es un comodin. significa que me va a devolver todas las tablas que empiecen con la letra p.
+
+            while(rs.next()){
+               System.out.println("Tabla: " + rs.getString("TABLE_NAME"));
+            }
+
+            //lista de columnas de productos:
+
+            System.out.println("");
+            System.out.println("Campos de productos:");
+
+            rs1 = metadatos.getColumns(null, null, "PRODUCTOS", null);
+
+            while(rs1.next()){
+               System.out.println("Nombre Columna: " + rs1.getString("COLUMN_NAME"));
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally{
+            try {
+                miconexion.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
