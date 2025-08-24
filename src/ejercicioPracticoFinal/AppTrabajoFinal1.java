a30eb3ad3c1c72d96def1a39dd8e1fbed225ef64
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-08-24 15:49:34 -0300

    me trae los metadatos

--- src/ejercicioPracticoFinal/AppTrabajoFinal1.java
+++ src/ejercicioPracticoFinal/AppTrabajoFinal1.java
@@ -4,9 +4,13 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.util.ArrayList;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 
 
@@ -38,10 +42,22 @@ class Lamina extends JPanel{ //CLASE LAMINA
          setLayout(new BorderLayout());
          combo1=new JComboBox();
          area_texto=new JTextArea(8,20);
-         add(combo1, BorderLayout.NORTH);
-         add(area_texto, BorderLayout.CENTER);
+
          conectarConBaseDatos(); //LLAMO A ESTE MÉTODO PARA QUE ME CONECTE A LA BASE DE DATOS
          obtenerTablas(); //LLAMO A ESTE MÉTODO PARA QUE ME CARGUE LAS TABLAS
+
+         combo1.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(java.awt.event.ActionEvent e) {
+                 // Acción a realizar cuando se selecciona un elemento del combo
+                 String nombreTablas=((String)combo1.getSelectedItem()); // OBTENGO EL NOMBRE DE LA TABLA SELECCIONADA por el usuario
+                 mostrarDatosTabla(nombreTablas); //LLAMO A ESTE MÉTODO PARA QUE ME MUESTRE LOS DATOS DE LA TABLA SELECCIONADA
+             }
+         });
+         add(combo1, BorderLayout.NORTH);
+
+         add(area_texto, BorderLayout.CENTER);
+         
     }
 
     public void conectarConBaseDatos(){ // ESTE MÉTODO ME CONECTA A LA BASE DE DATOS
@@ -61,7 +77,7 @@ class Lamina extends JPanel{ //CLASE LAMINA
         ResultSet miResultet=null; 
         try{
 
-            DatabaseMetaData datosBDD=miConexion.getMetaData(); //ESTE OBJETO ME DA INFORMACIÓN DE LA BASE DE DATOS
+            DatabaseMetaData datosBDD=miConexion.getMetaData(); //ESTE OBJETO ME LEE LAS TABLAS DE LA BASE DE DATOS
             //El método getTables me devuelve las tablas de la base de datos
             miResultet=datosBDD.getTables(null,null,null,null);
             while(miResultet.next()){
@@ -71,6 +87,44 @@ class Lamina extends JPanel{ //CLASE LAMINA
             e.printStackTrace();
     }
     }
+
+    public void mostrarDatosTabla(String Tabla){ // ESTE MÉTODO ME MUESTRA LOS DATOS DE LA TABLA SELECCIONADA
+       ArrayList<String> datos=new ArrayList<String>();
+       String consulta="SELECT * FROM " + Tabla; //CONSULTA PARA OBTENER TODOS LOS DATOS DE LA TABLA SELECCIONADA
+       try{
+
+        area_texto.setText(""); //LIMPIO LA CAJA DE TEXTO CADA VEZ QUE SE SELECCIONA UNA TABLA DIFERENTE
+        Statement miStatement=miConexion.createStatement(); //OBJETO QUE ME PERMITE EJECUTAR SENTENCIAS SQL PREPARADAS
+        ResultSet miResultSet=miStatement.executeQuery(consulta); //para que ejecute la consulta
+        //objetivo del resultet: almacenar el resultado de la consulta (saber cuantas filas y columnas tiene el resultado)
+        ResultSetMetaData rsBDD=miResultSet.getMetaData(); //OBJETO QUE ME PERMITE SABER CUANTAS COLUMNAS TIENE LA TABLA
+        for (int i=1;i<=rsBDD.getColumnCount();i++){ //RECORRO TODAS LAS COLUMNAS
+            datos.add(rsBDD.getColumnLabel(i)); //AÑADO EL NOMBRE DE CADA COLUMNA AL ARRAYLIST
+        }
+        //hasta aca, tengo los nombres de las columnas en el arraylist
+        //ahora me tiene que recorrer el resultset para obtener los datos del arraylist 
+
+
+
+        while(miResultSet.next()){ //RECORRO TODAS LAS FILAS
+            for (String nombreColumna:datos){ //RECORRO EL ARRAYLIST
+                area_texto.append(miResultSet.getString(nombreColumna) + " "); //AÑADO LOS DATOS DE CADA COLUMNA A LA CAJA DE TEXTO 
+            }
+            area_texto.append("\n"); //SALTO DE LINEA PARA QUE LOS DATOS DE CADA FILA SALGAN EN UNA LINEA DIFERENTE
+        }
+
+        /* CUANDO EL BUCLE ENTRA EN EL WHILE, SE LEE EL PRIMER REGISTRO DEL RESULTSET
+         * LUEGO CON EL BUCLE FOR EACH, LE DECIMOS QUE NOS EXTRAIGA DEL ARRAYLIST EL NOMBRE DEL PRIMER CAMPO 
+         * Y LO QUE TIENE QUE HACER EL FOR ES:
+         * QUE SAQUE EL TEXTO (getString) DEL PRIMER CAMPO (nombreColumna) DEL RESULTSET (miResultSet)
+         */
+
+       }catch(Exception e){
+        e.printStackTrace();
+       }
+    }
+
+
     //VARIABLES
     private JTextArea area_texto;
     private JComboBox combo1;
